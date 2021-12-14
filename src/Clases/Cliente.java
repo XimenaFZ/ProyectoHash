698b6046670615550cf806a32360cc46214dcdd9
XimenaFlores <XimenaFlores@DESKTOP-LVPM7CI>
2021-12-14 16:09:01 -0600

    Se modificó las clases: Clientes, Hash.

--- src/Clases/Cliente.java
+++ src/Clases/Cliente.java
@@ -1,5 +1,31 @@
 package Clases;
 
 public class Cliente {
-    
+    private String nombre;
+    private int nc;
+    private int saldo;
+   
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String documento) {
+        this.nombre = documento;
+    }
+
+    public int getNCuenta() {
+        return nc;
+    }
+
+    public void setNCuenta(int nc) {
+        this.nc = nc;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    } 
 }
--- src/Clases/HASH.java
+++ src/Clases/HASH.java
@@ -1,4 +1,38 @@
 package Clases;
 
+import java.util.Enumeration;
+import java.util.Hashtable;
+import javax.swing.JOptionPane;
+
 public class HASH {
+    String nombre;
+    int nc;
+    int saldo;
+    
+    public int estado; 
+
+    static int funcion(int n, int m) {
+        return ((n + 1) % m);
+    }
+
+    public static void agregarCliente(HASH[] h, int m, int n, String nombre, int saldo) {
+        boolean i = false;
+        int j = funcion(n, m);
+        do {
+            if (h[j].estado == 0 || h[j].estado == 1) {
+                h[j].nc = n;
+                h[j].saldo = saldo;
+                h[j].nombre = nombre;
+                h[j].estado = 2;
+                i = true;
+            } else {
+                j++;
+            }
+        } while (j < m && !i);
+        if (i) {
+            javax.swing.JOptionPane.showMessageDialog(null, "¡Elemento insertado con éxito!");
+        } else {
+            javax.swing.JOptionPane.showMessageDialog(null, "¡Tabla llena!");
+        }
+    }
 }
